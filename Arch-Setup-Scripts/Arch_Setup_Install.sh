2abe76d1c99248f6fa4ef2e82e48eebea5c06c25
4r6h <108066623+4r6h@users.noreply.github.com>
2023-01-10 20:55:42 +0600

    Update Arch_Setup_Install.sh

--- Arch-Setup-Scripts/Arch_Setup_Install.sh
+++ Arch-Setup-Scripts/Arch_Setup_Install.sh
@@ -113,7 +113,6 @@ if [[ -z ${setup_ans} || ${setup_ans} == "y" || ${setup_ans} == "Y" ]]; then
 	elif [[ -z ${aur_ans} || ${aur_ans} == "y" || ${aur_ans} == "Y" ]]; then
 		echo -e "${BYellow}[ * ]Installing paru as the AUR Helper${End_Colour}"
 		git clone https://aur.archlinux.org/paru.git
-		sudo chown -R "${USER}":"${USER}" paru
 		cd ./paru || exit
 		makepkg -si 
 	fi
